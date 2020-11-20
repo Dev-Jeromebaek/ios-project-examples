3378304870c11c23c557d6a333dc75d8026b8d14
seungyeob <superior051@icloud.com>
2020-11-20 16:44:39 +0900

    feat: Add new tutorial project 'autolayout_constraint_priority'

---
+++ autolayout_constraint_priority/autolayour_constraint_priority/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  autolayour_constraint_priority
+//
+//  Created by 백승엽 on 2020/11/20.
+//
+
+import UIKit
+
+//MARK: - 컨스트레인트 우선순위
+// - 각 컨스트레인트에 대한 우선순위
+// - 앵커가 서로 충돌 될때 우선순위가 낮은거는 무시
+// - 1000 이 가장 높다
+
+//MARK: Hugging
+// - 안으로 끌어안기
+// - 공간이 남아서 당겨지는 상황
+
+//MARK: Compression Resistance
+// - 밖으로 밀어내기
+// - 공간이 없어서 쪼그라드는 상황
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
